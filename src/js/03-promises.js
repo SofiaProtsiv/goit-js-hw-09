abd73443dd901130c76e3014e89331a95ac730fa
Sofia Protsiv <prochivsofia@gmail.com>
2021-11-10 17:09:08 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,70 @@
-function createPromise(position, delay) {
+'use strict';
+
+import Notiflix from 'notiflix';
+import "notiflix/dist/notiflix-3.2.1.min.css";
+const refs = {
+  delay: document.querySelector('input[name="delay"]'),
+  step: document.querySelector('input[name="step"]'),
+  amount: document.querySelector('input[name="amount"]'),
+  button: document.querySelector('button'),
+}
+// let position = 0;
+
+// refs.button.addEventListener('click', onSubmitBtnClick);
+
+function onSubmitBtnClick() {
+
+}
+
+  const valueObject = {
+    delay,
+    step,
+    amount
+  };
+
+console.log(createDataObject())
+let position = 1;
+
+setTimeout(function createPromise(position, delay) {
+  position++;
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   }
-}
+  setTimeout(createPromise, 1000);
+}, 1000)
+//   createPromise(3, 1500)
+//   .then(({ position, delay }) => {
+//     Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
+// }
+
+//   if(valueObject.delay == "" || valueObject.step == "" || valueObject.amount == "" ){
+//     alert('pfgjdysvcnm gjkz')
+//   }
+//   return valueObject;
+// }
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   setTimeout(() => {
+//     return new Promise((resolve) => {
+//       setTimeout(() => {
+//         resolve(delay);
+//       }, delay);
+//       if (shouldResolve) {
+//         Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//       } else {
+//         Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//       }
+//     });
+//   });
+//   }
+  
+
+
