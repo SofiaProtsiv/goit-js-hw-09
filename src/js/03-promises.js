b10d3595df2bea68543e73a8447e4a72e4281e8c
Sofia Protsiv <prochivsofia@gmail.com>
2021-11-12 22:02:50 +0200

    The value of the promise is an object with position and delay properties

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -16,25 +16,25 @@ function onSubmitBtnClick(event) {
   const amount = parseInt(formElements.amount.value);
 
   for (let position = 1; position <= amount; position += 1) {   
-
-    createPromise(position, delay)
-      .then(result => Notiflix.Notify.success(result))
-      .catch(error => Notiflix.Notify.failure(error));
+    
+    createPromise({position, delay})
+      .then((position, delay) => Notiflix.Notify.success(position, delay))
+      .catch((position, delay) => Notiflix.Notify.failure(position, delay));
     
     delay += step;    
   }  
     form.reset();
 }
 
-function createPromise(position, delay) {  
+function createPromise({position, delay}) {  
   const shouldResolve = Math.random() > 0.3;
    
   return new Promise((resolve, reject) => {
       setTimeout(() => {
       if (shouldResolve) {
-        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        resolve(`✅ Fulfilled promise ${position} in ${delay} ms`);
       } else {
-        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+        reject(`❌ Rejected promise ${position} in ${delay} ms`);
       }
     }, delay);
   });
