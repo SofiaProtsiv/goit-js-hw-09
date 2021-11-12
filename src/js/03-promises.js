8c2e9e005ef6cf9bcfbe7a5a596f382b9e3fafec
Sofia Protsiv <prochivsofia@gmail.com>
2021-11-12 23:14:38 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -16,25 +16,31 @@ function onSubmitBtnClick(event) {
   const amount = parseInt(formElements.amount.value);
 
   for (let position = 1; position <= amount; position += 1) {   
-    
-    createPromise({position, delay})
-      .then((position, delay) => Notiflix.Notify.success(position, delay))
-      .catch((position, delay) => Notiflix.Notify.failure(position, delay));
-    
+
+      createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
     delay += step;    
   }  
     form.reset();
 }
 
-function createPromise({position, delay}) {  
+function createPromise(position, delay) {  
+  
   const shouldResolve = Math.random() > 0.3;
-   
+  const data = {position, delay};
+
   return new Promise((resolve, reject) => {
       setTimeout(() => {
       if (shouldResolve) {
-        resolve(`✅ Fulfilled promise ${position} in ${delay} ms`);
+        resolve(data);
       } else {
-        reject(`❌ Rejected promise ${position} in ${delay} ms`);
+        reject(data);
       }
     }, delay);
   });
