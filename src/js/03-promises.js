5cea0262fe9654a6b79d7d68532ba3c5c2962885
Sofia Protsiv <prochivsofia@gmail.com>
2021-11-12 11:08:36 +0200

    button disabled. counter works. step++

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,6 +2,7 @@
 
 import Notiflix from 'notiflix';
 import "notiflix/dist/notiflix-3.2.1.min.css";
+
 const refs = {
   delay: document.querySelector('input[name="delay"]'),
   step: document.querySelector('input[name="step"]'),
@@ -14,22 +15,25 @@ let data;
 let delay;
 let step;
 let amount;
-let counter;
 refs.button.addEventListener('click', onSubmitBtnClick);
-refs.form.addEventListener('input', onMessageInput);
+refs.form.addEventListener('input', onInput);
 
 function onSubmitBtnClick(event) {
-    setTimeout(function onDelay() {
-      counter += 1;
-      const shouldResolve = Math.random() > 0.3;
-        if (shouldResolve) {
-          Notiflix.Notify.success(`✅ Fulfilled promise ${counter} in ${data.delay}ms`);
-        } else {
-          Notiflix.Notify.failure(`❌ Rejected promise ${counter} in ${data.delay}ms`);
-        }
-      setTimeout(onDelay, data.step);
-    }, data.delay);
-}
+  event.preventDefault();  
+
+  let delay = parseInt(data.delay);
+  const step = parseInt(data.step);
+  const amount = data.amount;
+
+  for (let position = 1; position <= amount; position += 1) {   
+
+    createPromise(position, delay)
+      .then(result => Notiflix.Notify.success(result))
+      .catch(error => Notiflix.Notify.failure(error));
+    
+    delay += step;    
+    }  
+  }
 
 function createDataObject (delay, step, amount) {
   const valueObject = {
@@ -39,7 +43,7 @@ function createDataObject (delay, step, amount) {
   };
   return valueObject;
 }
-function onMessageInput (event) {
+function onInput (event) {
   if(event.target.name === "delay"){
     delay = event.target.value;
   }
@@ -49,7 +53,24 @@ function onMessageInput (event) {
   if(event.target.name === "amount"){
     amount = event.target.value;
   }
+
+  if (refs.delay.value.length >= 1 && refs.step.value.length >= 1 && refs.amount.value.length >= 1) {
+    refs.button.removeAttribute('disabled');
+  }
+
   data = createDataObject(delay, step, amount);
 };
 
-
+function createPromise(position, delay) {  
+  const shouldResolve = Math.random() > 0.3;
+   
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+};
